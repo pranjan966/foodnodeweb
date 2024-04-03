bda47293f43a5257bb50854a17e427f1e1aa4008
onkarsagar111 <onkarsagar111@gmail.com>
2024-04-04 01:43:32 +0530

    Setup done for typescript, babel, webpack, babel-laoder, react

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type AppProps = {
+
+}
+
+const App: React.FC<AppProps> = (props) => {
+  return (
+    <div id='app-root'>
+      Hi there
+    </div>
+  )
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const container = document.getElementById('root') as any;
+ReactDOM.render(<App />, container);
